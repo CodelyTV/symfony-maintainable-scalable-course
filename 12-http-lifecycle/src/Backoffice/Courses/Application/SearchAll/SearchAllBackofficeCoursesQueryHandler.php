3355f87d77ff49a8b53d0e811e01752d3036d501
Dani Santamaría <santaka87@gmail.com>
2021-06-07 14:49:10 +0200

    [1.2] Symfony 5.2 HTTP Lifecycle

---
+++ 12-http-lifecycle/src/Backoffice/Courses/Application/SearchAll/SearchAllBackofficeCoursesQueryHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodelyTv\Backoffice\Courses\Application\SearchAll;
+
+use CodelyTv\Backoffice\Courses\Application\BackofficeCoursesResponse;
+use CodelyTv\Shared\Domain\Bus\Query\QueryHandler;
+
+final class SearchAllBackofficeCoursesQueryHandler implements QueryHandler
+{
+    public function __construct(private AllBackofficeCoursesSearcher $searcher)
+    {
+    }
+
+    public function __invoke(SearchAllBackofficeCoursesQuery $query): BackofficeCoursesResponse
+    {
+        return $this->searcher->searchAll();
+    }
+}
