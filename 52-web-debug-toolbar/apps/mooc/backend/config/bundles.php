d1b6d73b6c1c5964f0b476a5f6f3eaab23fb13d7
Dani Santamaría <santaka87@gmail.com>
2021-06-14 17:11:30 +0200

    [5.2] Add web profiler

--- 52-web-debug-toolbar/apps/mooc/backend/config/bundles.php
+++ 52-web-debug-toolbar/apps/mooc/backend/config/bundles.php
@@ -2,6 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                              => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class                          => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class                                        => ['all' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     //    WouterJ\EloquentBundle\WouterJEloquentBundle::class                                => ['test' => true]
 ];
