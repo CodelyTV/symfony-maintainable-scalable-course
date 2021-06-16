a94b327876b17fef807b8c1e1f145895554b074b
Dani Santamaría <santaka87@gmail.com>
2021-06-16 18:40:10 +0200

    [4.1] unmaintainable exception handling

--- 41-error-handling/apps/mooc/backend/src/Controller/CoursesCounter/CoursesCounterGetController.php
+++ 41-error-handling/apps/mooc/backend/src/Controller/CoursesCounter/CoursesCounterGetController.php
@@ -7,28 +7,56 @@ namespace CodelyTv\Apps\Mooc\Backend\Controller\CoursesCounter;
 use CodelyTv\Mooc\CoursesCounter\Application\Find\CoursesCounterResponse;
 use CodelyTv\Mooc\CoursesCounter\Application\Find\FindCoursesCounterQuery;
 use CodelyTv\Mooc\CoursesCounter\Domain\CoursesCounterNotExist;
-use CodelyTv\Shared\Infrastructure\Symfony\ApiController;
+use CodelyTv\Shared\Domain\Bus\Query\QueryBus;
+use InvalidArgumentException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
-final class CoursesCounterGetController extends ApiController
+final class CoursesCounterGetController
 {
-    public function __invoke(): JsonResponse
-    {
-        /** @var CoursesCounterResponse $response */
-        $response = $this->ask(new FindCoursesCounterQuery());
-
-        return new JsonResponse(
-            [
-                'total' => $response->total(),
-            ]
-        );
+    public function __construct(
+        private QueryBus $queryBus,
+    ) {
     }
 
-    protected function exceptions(): array
+    public function __invoke(): JsonResponse
     {
-        return [
-            CoursesCounterNotExist::class => Response::HTTP_NOT_FOUND,
-        ];
+        try {
+            /** @var CoursesCounterResponse $response */
+            $response = $this->queryBus->ask(new FindCoursesCounterQuery());
+            return new JsonResponse(
+                [
+                    'total' => $response->total(),
+                ]
+            );
+        } catch (CoursesCounterNotExist $exception) {
+            $code = Response::HTTP_NOT_FOUND;
+            return new JsonResponse(
+                [
+                    'code'    => $code,
+                    'message' => $exception->getMessage(),
+                ],
+                $code
+            );
+        } catch (InvalidArgumentException $exception) {
+            $code = Response::HTTP_BAD_REQUEST;
+            return new JsonResponse(
+                [
+                    'code'    => $code,
+                    'message' => $exception->getMessage(),
+                ],
+                $code
+            );
+        } catch (Throwable $exception) {
+            $code = Response::HTTP_INTERNAL_SERVER_ERROR;
+            return new JsonResponse(
+                [
+                    'code'    => $code,
+                    'message' => $exception->getMessage(),
+                ],
+                $code
+            );
+        }
     }
 }
