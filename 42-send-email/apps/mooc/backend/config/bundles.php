757743dc32e06afaf14d47e11d782cadb425f72e
Dani Santamaría <santaka87@gmail.com>
2021-06-16 20:30:38 +0200

    [4.2] Add web profiler

--- 42-send-email/apps/mooc/backend/config/bundles.php
+++ 42-send-email/apps/mooc/backend/config/bundles.php
@@ -2,6 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                              => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class                          => ['test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class                                        => ['test' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     //    WouterJ\EloquentBundle\WouterJEloquentBundle::class                                => ['test' => true]
 ];
