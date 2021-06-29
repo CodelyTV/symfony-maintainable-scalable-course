ad0a377297f770ac1feff130fcc82ea9d15bfe90
Dani Santamaría <santaka87@gmail.com>
2021-06-29 13:42:54 +0200

    [8.1] Use mailer to send certificate

--- 71-jwt-tokens/src/Controller/SendCertificatePostController.php
+++ 71-jwt-tokens/src/Controller/SendCertificatePostController.php
@@ -7,11 +7,15 @@ namespace App\Controller;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 final class SendCertificatePostController
 {
-    public function __construct(private JWTEncoderInterface $jwtEncoder)
-    {
+    public function __construct(
+        private JWTEncoderInterface $jwtEncoder,
+        private MailerInterface $mailer
+    ) {
     }
 
     public function __invoke(): Response
@@ -20,6 +24,13 @@ final class SendCertificatePostController
             'student_id' => '5',
             'course_id' => '3'
         ]);
+
+        $email = (new Email())
+            ->from('hello@codely.tv')
+            ->to('dani@codely.tv')
+            ->subject('Codely course certification!')
+            ->html("<p><a href=\"http://localhost:8000/certificate?cert=$token\">View certificate</a></p>");
+        $this->mailer->send($email);
 
         return new JsonResponse(['token' => $token]);
     }
